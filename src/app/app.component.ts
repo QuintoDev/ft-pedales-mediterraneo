04cf981c0b31adaf3c392ea64a4b19b72b79977a
Hernan Quintero <hernanquintero@Hernans-Mac-mini.local>
2024-10-29 22:03:13 -0500

    add: se añade componente de eventos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EventosComponent } from "./eventos/eventos.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EventosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ft-pedales-mediterraneo';
+  title = 'Pedales del Mediterraneo';
 }
